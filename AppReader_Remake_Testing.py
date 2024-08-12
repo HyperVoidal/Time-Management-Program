cd5f632e38bc26a71be78c3eac0d05ee0e26a103
HyperVoidal <129457072+HyperVoidal@users.noreply.github.com>
2024-08-12 13:26:34 +1000

    Main files uploaded

---
+++ AppReader_Remake_Testing.py
@@ -0,0 +1,178 @@
+import pyautogui
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import threading
+import json
+import traceback
+
+truepath = os.path.dirname(os.path.abspath(__file__))
+path = truepath  + "\\static\\AppReaderData.json"
+fig, ax = plt.subplots(figsize=(7,4))
+columngraphpath = truepath + "\\static\\Column.png"
+timedisplay = "Seconds"
+
+class AppReader():
+    def __init__(self):
+        self.start_time = time.time()
+        self.appsrunning = []
+        self.apptime = {}
+        self.appslist = {}
+
+    def AppNameEdit(self):
+        #Add another name editor for app names that are part of the same action but have different names
+        #e.g. mozilla firefox - new tab, and mozilla firefox - stackoverflow, should become the same name
+        modified_apps = []  # Initialize a new list for modified titles
+        for x in self.appsrunning:
+            titletest = x
+            if titletest in ["", " ", "    ", " - "]:  # Simplified check for empty or specific strings
+                titletest = "UNKNOWN APP"
+            if " - " in titletest:
+                thetitletest = titletest.split(" - ")
+                titletest = thetitletest[-1]
+            if "\\" in titletest:
+                thetitletest = titletest.split("\\")
+                titletest = thetitletest[-1]
+            if " - " not in titletest:
+                titletest = titletest
+            if len(titletest) > 20:
+                thetitletest = titletest[:17] + "..."
+                titletest = thetitletest
+            modified_apps.append(titletest)  # Append the modified title to the new list
+        self.appsrunning.clear()
+        self.appsrunning = modified_apps  # Replace the original list with the modified list
+    
+    def AppRunning(self):
+        self.appsrunning.extend([window.title for window in pyautogui.getAllWindows()])
+        self.appsrunning = [x for x in self.appsrunning if x.strip() and x != ""]
+        seen = set()
+        self.appsrunning = [x for x in self.appsrunning if not (x in seen or seen.add(x))]
+    
+    def UpdateRunningApps(self):
+        # Fetch the current list of running apps
+        current_running_apps = [window.title for window in pyautogui.getAllWindows()]
+        current_running_apps = [x for x in current_running_apps if x.strip() and x != ""]
+
+        # Filter out apps that are no longer running from self.appsrunning
+        # Do not modify self.apptime here to ensure time persists
+        self.appsrunning = [app for app in self.appsrunning if app in current_running_apps]
+
+    def AppTimer(self):
+        current_time = round(time.time())  # Ensure current_time is rounded at the source
+        for app in self.appsrunning:
+            if app in self.apptime:
+                # Validate appchoose is a dictionary and has 'cur_time'
+                if isinstance(self.apptime[app], dict) and 'cur_time' in self.apptime[app]:
+                    appchoose = self.apptime[app]
+                    # Ensure elapsed_time is rounded immediately after calculation
+                    elapsed_times = int(round(current_time - appchoose['cur_time']))
+                    elapsed_time = round(elapsed_times, 0)
+                    self.apptime[app]['elapsed'] = elapsed_time  # Convert to int to ensure compatibility
+                else:
+                    print(f"Error: appchoose for {app} is not properly formatted. Current value: {self.apptime[app]}")
+            else:
+                # For new apps, ensure 'cur_time' is rounded before being set
+                self.apptime[app] = {'cur_time': round(current_time, 0), 'elapsed': 0}  # current_time is already rounded
+                
+    def AppRelease(self):
+        for app_name, time in self.apptime.items():
+            # Update the time for the app in self.appslist
+            self.appslist[app_name] = (f"{time['elapsed']} seconds")
+
+def update_graph(timedisplay):
+    try:
+        ax.clear()
+        app_names = list(AppReader.appslist.keys())
+        for i in (AppReader.appslist.values()):
+            values = i.split(" ")
+            value = values[0]
+            maximum = max([int(round(float(value.split(" ")[0]), 0)) for value in AppReader.appslist.values()])
+            if maximum > 60 and maximum < 3600:
+                timedisplay = "Minutes"
+                break
+            elif maximum > 3600:
+                timedisplay = "Hours"
+                break
+            elif maximum > 86400:
+                timedisplay = "Days"
+                break
+            else:
+                pass
+            
+        if timedisplay == "Seconds":
+            times = [int(round(float(time.split(" ")[0]), 0)) for time in AppReader.appslist.values()]
+        elif timedisplay == "Minutes":
+            times = [(int(round(float(time.split(' ')[0]), 0)))/60 for time in AppReader.appslist.values()]
+        elif timedisplay == "Hours":
+            times = [(int(round(float(time.split(" ")[0]), 0)))/3600 for time in AppReader.appslist.values()]
+        elif timedisplay == "Days":
+            times = [(int(round(float(time.split(" ")[0]), 0)))/86400 for time in AppReader.appslist.values()]
+        else:
+            print("Error: Invalid time display. Defaulting to hours.")
+            times = [(int(round(float(time.split(" ")[0]), 0)))/3600 for time in AppReader.appslist.values()]
+            timedisplay = "Hours"
+                
+        color_indices = np.arange(len(app_names))
+        cmap = plt.colormaps.get_cmap("viridis")
+        colors = [cmap(index*15) for index in color_indices]
+        for i in range(len(app_names)):
+            for i in range(len(app_names)):
+                if len(app_names[i]) > 23:
+                    theappnames = app_names[i][:20] + "..."
+                    app_names[i] = theappnames
+
+        ax.bar(app_names, times, color=colors, edgecolor="black")
+        ax.set_xlabel('App Name')
+        ax.set_ylabel(f'Time ({timedisplay})')
+        ax.set_title('App Usage Time')
+        plt.grid(True, which="both", linestyle="--", linewidth=0.5, color="gray", axis="y")
+        plt.xticks(rotation = 45, ha="right")
+        plt.subplots_adjust(left=0.2,bottom=0.4, top = 0.9, right = 0.9)
+        plt.savefig(columngraphpath)
+    except Exception as e:
+        print("GRAPH ERROR:")
+        print(e)
+        print(traceback.format_exc())
+        time.sleep(2)
+        pass
+
+    return timedisplay
+    
+    
+def scan():
+    while True:
+        AppReader.AppRunning(AppReader)
+        AppReader.UpdateRunningApps(AppReader)
+        AppReader.AppNameEdit(AppReader)
+        AppReader.AppTimer(AppReader)
+        AppReader.AppRelease(AppReader)
+
+#Update json and store json need to be edited to account for apptime library changes
+def UpdateJson(apptime, path):
+    with open (f"{path}", "w") as f:
+        json.dump(apptime, f)
+    
+        
+AppReader.__init__(AppReader)
+
+#Load the previously stored json data
+try:
+    with open(f"{path}", "r") as f:
+        appnamelist = json.load(f)
+        AppReader.appsrunning = list(appnamelist.keys())
+        AppReader.apptime.update(dict(appnamelist))
+        AppReader.apptime = appnamelist
+        
+except:
+    pass
+thread = threading.Thread(target=scan)
+    
+thread.start()
+
+time.sleep(5)
+while True:
+    update_graph(timedisplay)
+    UpdateJson(AppReader.apptime, path)
+    time.sleep(0.5)
+    
---
+++ Learning Flask.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, render_template
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, Length
+import json
+from subprocess import Popen
+#UTILISE POPEN MULTITHREADING FOR THE APP READER SCRIPT
+
+try:
+    Popen("python AppReader_Remake_Testing.py")
+except:
+    pass
+DataStoreTasks = {}
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the key oooh'
+
+class TaskForm(FlaskForm): 
+    Task = StringField('Name', validators=[InputRequired()])
+    TaskDesc = TextAreaField('Description', validators=[InputRequired(), Length(max=100)])
+
+    
+
+@app.route('/Page_1/', methods=['POST', 'GET'])
+def page_1():
+    if request.method == 'POST':
+        button_value = request.form.get('button')
+        if button_value == 'Page 2':
+            form = TaskForm()
+            return render_template('Page_2.html', form=form, DataStoreTasks=DataStoreTasks)
+    return render_template('Page_1.html')
+
+
+
+@app.route('/Page_2/', methods=['POST', 'GET'])
+def page_2():
+    form = TaskForm()
+    
+    if request.method == 'POST':
+        button_value = request.form.get('button')
+        if button_value == 'Page 1':
+            return render_template('Page_1.html')
+        elif button_value == 'submit':
+            # Extract form data into the DataStoreTasks dictionary
+            task = request.form.get('Task')
+            task_desc = request.form.get('TaskDesc')
+            if task and task_desc:
+                taskcheck = task_desc.split(" ")
+                for i in range(len(taskcheck)):
+                    if len(str(taskcheck[i])) < 30:
+                        pass
+                    elif len(str(taskcheck[i])) > 30 and len(str(taskcheck[i])) < 60:
+                        taskcheck[i] = taskcheck[i][:30] + "- " + taskcheck[i][30:]
+                    elif len(str(taskcheck[i])) > 60 and len(str(taskcheck[i])) < 90:
+                        taskcheck[i] = taskcheck[i][:30] + "- " + taskcheck[i][30:60] + "- " + taskcheck[i][60:]
+                    elif len(str(taskcheck[i])) > 90:
+                        taskcheck[i] = taskcheck[i][:30] + "- " + taskcheck[i][30:60] + "- " + taskcheck[i][60:90] + "- " + taskcheck[i][90:]
+                    task_desc = " ".join(taskcheck)
+                    
+                DataStoreTasks[task] = task_desc
+                print(DataStoreTasks)  # For debugging purposes
+            else:
+                print("Task or TaskDesc is missing")  # Debugging message for missing data
+            
+            with open ('TaskStorage.json', 'w') as f:
+                json.dump(DataStoreTasks, f)
+            return render_template('Page_2.html', form=form, DataStoreTasks=DataStoreTasks)
+    
+    return render_template('Page_2.html', form=form, DataStoreTasks=DataStoreTasks)
+
+
+@app.route('/home', methods=['POST', 'GET'])
+def home():
+    if request.method == 'POST':
+        button_value = request.form.get('button')  # Assuming the name of the button input is 'button'
+        if button_value == 'Page 1':
+            return render_template('Page_1.html')
+        elif button_value == 'Page 2':
+            form = TaskForm()
+            return render_template('Page_2.html', form=form, DataStoreTasks=DataStoreTasks)
+    # If GET or no recognized button value, stay on the login page or handle accordingly
+    return 'Button Error'
+
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+    data = request.get_json()
+    task = data.get('task')
+
+    if task in DataStoreTasks:
+        del DataStoreTasks[task]
+        with open('TaskStorage.json', 'w') as f:
+            json.dump(DataStoreTasks, f)
+        return {'status': 'success'}, 200
+    else:
+        return {'status': 'task not found'}, 404
+
+if __name__ == '__main__':
+    try:
+        with open ('TaskStorage.json', 'r') as f:
+            DataStoreTasks = json.load(f)
+    except:
+        print('No data in file')
+    app.run(debug=True)
